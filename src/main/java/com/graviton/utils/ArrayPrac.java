34b401e65d83fc93209f516250ff65857bb8fa88
MD BIPLOB <biplob@MDs-Mac-Pro.local>
2021-09-11 11:09:27 -0500

    Adding Excel utils

--- src/main/java/com/graviton/utils/ArrayPrac.java
+++ src/main/java/com/graviton/utils/ArrayPrac.java
@@ -4,22 +4,31 @@ public class ArrayPrac {
 
     public static void main(String[] args) {
 
+        //2D Array
         String[][] name = new String[3][3];
-        name[0][0] ="";
-        name[0][1] ="";
-        name[0][2] ="";
-        name[0][3] ="";
-        name[0][4] ="";
-        name[1][0] ="";
-        name[2][0] ="";
-        name[3][0] ="";
-        name[4][0] ="";
+        name[0][0] ="JD";
+        name[0][1] ="DAS";
+        name[0][2] ="Rafi";
+        name[1][0] ="NYC";
+        name[1][1] ="DC";
+        name[1][2] ="DC";
+        name[2][0] ="Lipi";
+        name[2][1] ="Taniya";
+        name[2][2] ="UT";
+
+        System.out.println(name);
+
+        for(int i=0; i<name.length; i++){
+            for(int j=0; j<name.length; j++){
+               // System.out.println(name[i][j]);
+            }
+
+        }
+
+        String[][] twoDnName = {{"JD","DAS", "RAFI"}, {"NYC","DC", "UT"}};
 
 
-
-
-
-
+        //Normal Array
         String[] names = new String[5];
         names[0] = "Rafi";
         names[1] = "Taniya";
@@ -27,6 +36,6 @@ public class ArrayPrac {
         names[3] = "Ashik";
         names[4] = "Nayeem";
 
-        System.out.println(names[0]);
+        //System.out.println(names[0]);
     }
 }
